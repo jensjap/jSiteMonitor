8736dd0ab70acc47d703036c22312b00f548e9d5
Jens Jap <jensjap@gmail.com>
2014-02-03 14:30:55 -0500

    functional

--- jSiteMonitor.py
+++ jSiteMonitor.py
@@ -2,6 +2,7 @@ import ConfigParser
 import os
 import logging
 import urllib
+import smtplib
 
 from urlparse import urlparse
 
@@ -17,45 +18,44 @@ Adapted from: https://github.com/davetromp/SiteMonitor/blob/master/SiteMonitor.p
 class monitor(object):
     """representation of a site up or down monitor"""
 
-    def __init__(self, site_url, prevStatus, lof_emails=[]):
+    def __init__(self, site_url, prevStatus, smtp_info, lof_emails=[]):
         self.site_url = site_url
         self.hostname = urlparse(site_url).hostname
-        self.lof_emails = lof_emails
         self.prevStatus = prevStatus
         self.currStatus = prevStatus
+        self.lof_emails = lof_emails
+        self.smtp_info = smtp_info
 
     def checkStatus(self):
-        self.logger = logging.getLogger('root.' + self.site_url)
+        self.logger = logging.getLogger('root.' + self.hostname)
         try:
             Gstatus = self.getStatus('http://www.google.com') # See if we can hit google
         except Exception as e:
-            self.logger.info("Can't even hit www.google.com. Better luck next time")
+            self.logger.info("Unexpected error while accessing www.google.com: " + e)
+            return
         if Gstatus != 200:
             self.logger.info("Can't even hit www.google.com. Better luck next time. Returned with" +
                              Gstatus)
             return
 
-        self.logger.info("Checking status of %s", self.site_url)
-        try:
-            status = self.getStatus(self.site_url)
-        except IOError as e:
-            status = 0
+        self.logger.info("Checking status of %s", self.hostname)
+        status = self.getStatus(self.site_url)
         if status != 200 and self.prevStatus != 'down':
-            self.logger.info("%s is down. Site status is %s" % (self.site_url, status))
+            msg = "%s is down. Site status returned: %s" % (self.hostname, status)
+            self.logger.info(msg)
+            self.sendEmail(msg) # Notify recipients that the site went down.
             self.currStatus = 'down'
-            self.sendEmail(0) # Notify recipients that site just went down
         elif status == 200 and self.prevStatus == 'down':
-            self.logger.info("%s is up again. Site status is %s" % (self.site_url, status))
+            msg = "%s is up again. Site status returned: %s" % (self.hostname, status)
+            self.logger.info(msg)
+            self.sendEmail(msg) # Notify recipients that site came back up!
             self.currStatus = 'up'
-            self.sendEmail(1) # Notify recipients that site came back up!
 
     def getStatus(self, url):
         """
         makes a request to an url and returns the header status.
         returns zero if url does not exist.
         """
-        a = urllib.urlopen(url)
-        print(a.getcode())
         try:
             a = urllib.urlopen(url)
             return a.getcode()
@@ -63,9 +63,26 @@ class monitor(object):
             self.logger.info(url + ' did not return any header status.')
             return 0
 
-    def sendEmail(self, code):
+    def sendEmail(self, msg, subject="Site down / up again message"):
         """0: site just went down, 1: site just came back up"""
-        print('Send email to', self.lof_emails)
+        to_addr = self.lof_emails
+        server = self.smtp_info['server']
+        from_addr = self.smtp_info['from_addr']
+        username = self.smtp_info['username']
+        password = self.smtp_info['password']
+
+        m = "From: %s\r\nTo: %s\r\nSubject: %s\r\nX-Mailer: My-Mail\r\n\r\n" % (from_addr, ", ".join(to_addr), subject)
+
+        try:
+            server = smtplib.SMTP(server)
+            server.starttls()
+            server.login(username,password)
+            server.sendmail(from_addr, to_addr, m+msg)
+            server.quit()
+            self.logger.debug('Email send to' + ", ".join(to_addr))
+        except Exception as e:
+            self.logger.debug('Email not send: ' + str(e))
+
 
 
 def monitorFactory(config):
@@ -73,6 +90,12 @@ def monitorFactory(config):
 
     lof_monitors = []
     lof_sites = []
+    smtp_info = {}
+
+    smtp_info['server'] = config.get('Email', 'server')
+    smtp_info['username'] = config.get('Email', 'username')
+    smtp_info['password'] = config.get('Email', 'password')
+    smtp_info['from_addr'] = config.get('Email', 'from')
 
     sites_path = config.get('Lists', 'sites_path')
 
@@ -92,7 +115,7 @@ def monitorFactory(config):
         try:
             prevStatus = config.get('Site status', hostname)
         except ConfigParser.NoOptionError as e:
-            updateConfig('./config.ini', 'Site status', hostname, '')
+            updateConfig('./config.ini', 'Site status', hostname, 'up')
         logger.debug("%s was %s last time it was checked" % (hostname, prevStatus))
 
         # Find the list of email recipients for this site url
@@ -104,7 +127,7 @@ def monitorFactory(config):
         logger.debug("Alert for %s need to be sent to %s" % (hostname, lof_emails))
 
         # Instantiate monitor object and append to list of monitors
-        lof_monitors.append(monitor(site, prevStatus, lof_emails))
+        lof_monitors.append(monitor(site, prevStatus, smtp_info, lof_emails))
 
     return lof_monitors
 
@@ -124,12 +147,15 @@ def main():
     logging.basicConfig(filename='log/jSiteMonitor.log',
             level=config.get('Settings', 'logging_level'),
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
     logging.info('Main program started')
 
     monitors = monitorFactory(config)
     for m in monitors:
         m.checkStatus()
         updateConfig(config_path, 'Site status', m.hostname, m.currStatus)
+
+    logging.info('Main program finished')
 
 
 
